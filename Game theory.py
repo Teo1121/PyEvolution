e23c1fdee4da7baaa0176a5d3732981f9e3a59ae
Teo Ferenac <teo.ferenac@gmail.com>
2019-08-23 22:11:59 +0200

    Added some comments

--- Game theory.py
+++ Game theory.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-numOfGoodies = [50] # 1
-numOfBadies = [50] # 4
+numOfGoodies = [50] # identity of Goodies = 1
+numOfBadies = [50] # identity of Badies = 3
 numOfFoodPairs = 500
 
 playersPos = [0 for i in range(numOfFoodPairs)]
@@ -11,25 +11,26 @@ gen = 0
 while gen < 500:
     for i in range(numOfBadies[-1]+numOfGoodies[-1]):
         foodPair = np.random.randint(numOfFoodPairs)
-        while playersPos[foodPair] == 2 or playersPos[foodPair] == 8 or playersPos[foodPair] == 5:
+        # no more than 2 players on one food pair
+        while playersPos[foodPair] == 2 or playersPos[foodPair] == 4 or playersPos[foodPair] == 6: 
             foodPair = foodPair+1 if foodPair+1 != numOfFoodPairs else 0
 
-        player = 1+3*(i%2)
+        player = 1+2*(i%2)
         playersPos[foodPair] += player
         
     tempGoodies = numOfGoodies[-1]
     tempBadies = numOfBadies[-1]
     for i in playersPos:
-        if i == 0 or i == 2:
+        if i == 0 or i == 2: # 0 = empty pos, 2 = two Goodies
             continue
-        if i == 1:
+        if i == 1: # 1 = one Goodie
             tempGoodies = min(tempGoodies+1,numOfFoodPairs)
-        if i == 4:
+        if i == 3: # 3 = one Badie
             tempBadies = min(tempBadies+1,numOfFoodPairs)
-        if i == 5:
+        if i == 4: # 4 = one Goodie and one Badie
             tempGoodies = max(tempGoodies-np.random.randint(2),0)
             tempBadies  = min(tempBadies+ np.random.randint(2),numOfFoodPairs)
-        if i == 8:
+        if i == 6: # 6 = two Badies
             tempBadies  = max(tempBadies-2,0)
             
            
